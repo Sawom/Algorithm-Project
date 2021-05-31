9ac015f8a46d98f3030196a87c3cbcf87e727b6f
Abdur Rashid Sawom <asawom250@gmail.com>
2021-05-31 13:36:25 +0600

    Update conferenceRoomSchedule.cpp

--- conferenceRoomSchedule.cpp
+++ conferenceRoomSchedule.cpp
@@ -1,6 +1,6 @@
-/**start = {1, 3, 0, 5, 8, 5}
-finish = {2, 4, 6, 7, 9, 9}
-output = {0 ,1, 3, 4}
+/**start = {1, 3, 0, 5, 5}
+finish = {2, 4, 6, 7, 9}
+output = {0 ,1, 3}
 */
 #include <bits/stdc++.h>
 using namespace std;
